baac769181094613ab009acb45622eb2a90b6760
Odoyao <josephfadegnon4@gmail.com>
2025-04-05 19:14:10 +0200

    chore: repush

--- src/pages/Acceuil/Acceuil.tsx
+++ src/pages/Acceuil/Acceuil.tsx
@@ -1,10 +1,18 @@
+import { useEffect } from "react";
+import './Acceuil.css';
+
 
 const Acceuil = () => {
-  return (
-    <div>
-      Acceuil
-    </div>
-  )
+
+    useEffect(() => {
+        document.title = 'Acceuil';
+    }, []);
+
+    return (
+        <div className="container">
+        Acceuil
+        </div>
+    )
 }
 
 export default Acceuil
