b76146f5e186bfb67ec99831ed28ca378b02c6a4
Joseph <josephfadegnon4@gmail.com>
2025-10-02 15:00:00 +0100

    fixed duraion issue on attraction

--- src/pages/Attractions/viewAttractions.tsx
+++ src/pages/Attractions/viewAttractions.tsx
@@ -284,10 +284,12 @@ const AttractionDetailPage = () => {
                       <MapPin className="w-5 h-5" />
                       <span>{attraction.location}</span>
                     </div>
-                    <div className="flex items-center gap-2">
-                      <Clock className="w-5 h-5" />
-                      <span>{attraction.duration}</span>
-                    </div>
+                    {attraction.duration != "" && (
+                      <div className="flex items-center gap-2">
+                        <Clock className="w-5 h-5" />
+                        <span>{attraction.duration}</span>
+                      </div>
+                    )}
                     {attraction.groupSize != 0 && (
                       <div className="flex items-center gap-2">
                         <Users className="w-5 h-5" />
